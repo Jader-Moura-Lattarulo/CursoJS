780580e5fe19907440bdb80853318869e4583231
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-16 17:37:07 -0300

    update

--- aula04/app.js
+++ aula04/app.js
@@ -1,7 +1,6 @@
 let nomeHomem = `João`; //string
 let timeDeFutebol; //variável declarada, mas não inicializada
 
-
 console.log(`${nomeHomem} nasceu em 1989.`);
 console.log(`Em 2004, ${nomeHomem} conheceu a Maria.`);
 console.log(`${nomeHomem} casou-se com Maria em 2012.`);
