5cff4e5cb7cd977e67a927662e9250ff5f69ad38
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-16 17:07:12 -0300

    update

--- aula01/app.js
+++ aula01/app.js
@@ -1,1 +1,5 @@
-console.log("Hello World!");
+console.log('"Hello World!"');
+console.log("'Hello World!'");
+console.log(`"Hello World!'`);
+
+console.log(123456789, 123123.123123, '"Jader"');
---
+++ aula02/comentarios.js
@@ -0,0 +1,14 @@
+//Escrever algum comentário qualquer
+console.log(`Olá mundo!`); //Aqui tem outro comentário
+console.log(`Linha 01`);
+console.log(`Linha 02`);
+//console.log(`Linha 03`);
+console.log(`Linha 04`);
+
+/**
+ * 
+ * 
+ * comentário em bloco
+ * 
+ * 
+ */
