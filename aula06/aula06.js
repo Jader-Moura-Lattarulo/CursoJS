e1a5cac522989faaf85f5cfbcc3150f145cc08d7
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-17 13:38:43 -0300

    Create aula06.js

---
+++ aula06/aula06.js
@@ -0,0 +1,14 @@
+/**
+ * Aritiméticos
+ * + Adição / concatenação
+ * - Subtração
+ * * Multiplicação
+ * ** Potenciação
+ * % Resto da divisão
+ * ++ incremento
+ * -- decremento 
+ * += contador - contador + passo
+ * NaN - Not a number
+ * parseInt - converte string para um number inteiro
+ * parseFloat - converte string para um number flutuante
+ */
