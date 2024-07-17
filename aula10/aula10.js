0675a8147577edc1037f251182ac83038ee3cbd6
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-17 18:04:36 -0300

    Create aula10.js

---
+++ aula10/aula10.js
@@ -0,0 +1,22 @@
+// IEEE 745-2008
+//Numbers
+let num1 = 1;
+let num2 = 2.5;
+let num3 = 12.54026565624;
+let num4 = 10;
+
+let num5 = 0.9;
+let num6 = 0.1;
+
+num5 += num6;
+
+console.log(num5);
+
+//num1 = num1.toString();
+//console.log(num1.toString() + num2);
+//console.log(num2.toString(2)); Númeração binária do num2
+//console.log(typeof num1);
+//console.log(num3.toFixed(2));
+//console.log(Number.isInteger(num4));
+//let temp = num1 * 'olá mundo';
+//console.log(Number.isNaN(temp));
