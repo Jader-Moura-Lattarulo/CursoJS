1cc0f6ef0e5ad248fdfb631020f81078b8150efb
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-17 14:13:27 -0300

    update

---
+++ aula07/aula07.js
@@ -0,0 +1,4 @@
+//alert, confirm e prompt
+alert(`Com a nossa msg`); //Não retorna nada.
+confirm(`Só para testar o comportamento`); //Retorna um valor booleano.
+prompt(`entre um valor`); //caputura um valor da entrada do usuário.
