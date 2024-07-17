68f8621ab54f935d32dca7a54a72b748bd53b593
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-17 12:41:08 -0300

    Create ex.js

---
+++ aula05/ex.js
@@ -0,0 +1,10 @@
+const firstName = "Jader";
+const lastName = "Moura Lattarulo"
+let age = 35;
+let weight = 92;
+const height = 1.65;
+let imc = weight/(height * height); //peso / (altura * altura)
+const birthYear = 1989;
+
+console.log(`${firstName} ${lastName} tem ${age} anos, pesa ${weight} kg, tem ${height} de altura e seu IMC é de: ${imc}.`);
+console.log(`${firstName} nasceu em ${birthYear}.`);
