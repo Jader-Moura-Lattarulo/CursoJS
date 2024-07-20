7eb6be3d74541eab3fa1f45bec6d78537c649cec
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-20 12:26:10 -0300

    Create aula11.js

---
+++ aula11/aula11.js
@@ -0,0 +1,13 @@
+let num1 = 9.43;
+let num2 = Math.floor(num1); //Arredonda para baixo
+num2 = Math.ceil(num1); //Arredonda para cima
+num2 = Math.round(num1); //Arredonda para o valor mais próximo
+Math.max(1, 2, 4, 654, 2456, 1345); //Devolve o maior número da sequência
+Math.min(1, 2, 4, 654, 2456, 1345); //Devolve o menor número da sequência
+Math.random(); //Cria um número aleatório
+Math.random() * (10 - 5) + 5; // Cria um número aleatório entre 10 e 5
+Math.PI //Retorna o valor de pi
+Math.pow(2,10); //Retorna a potenciação
+
+
+console.log(Math.random() * (10 - 5) + 5);
