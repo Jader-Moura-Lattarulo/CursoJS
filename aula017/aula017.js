265691a9c790e6c9b0e7019656ec9d852bfc13f1
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-21 16:34:49 -0300

    Update aula017.js

---
+++ aula017/aula017.js
@@ -0,0 +1,13 @@
+/**
+ * Operadores de comparação
+ * > maior que
+ * >= maior que ou igual a
+ * < menor que
+ * <= menor que ou igual a
+ * == igualdade (valor) ******* Não é utilizado
+ * === igualdade estrita (valor e tipo)
+ * != diferença (valor) ******* Não é utilizado
+ * !== fiferença estrita (valor e tipo)
+ * */
+const comp = 10 > 5
+console.log(comp); //true
