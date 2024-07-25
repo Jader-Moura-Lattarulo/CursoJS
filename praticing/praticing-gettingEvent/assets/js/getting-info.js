45aeaf202c6196e5a9aecd824bc562156728e5af
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-25 16:10:06 -0300

    update

--- praticing/praticing-gettingEvent/assets/js/getting-info.js
+++ praticing/praticing-gettingEvent/assets/js/getting-info.js
@@ -5,7 +5,16 @@ function kiMeter() {
     form.addEventListener('submit', function(event){
         event.preventDefault();
         console.log('Evento barrado');
+        setResult('bla bla bla');
     });
+
+    function setResult(msg){
+        const result = document.querySelector('#result');
+        result.innerHTML = "";
+        const paragraph = document.createElement('p');
+        paragraph.innerHTML = "Seu inceto";
+        result.appendChild(paragraph);
+    }
 
 }
 
