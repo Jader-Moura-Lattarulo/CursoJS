718aa7ea80a7afa73d832f024d6196d100ef6cdf
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-26 17:01:15 -0300

    Update app.js

--- aula019/app.js
+++ aula019/app.js
@@ -1,8 +1,11 @@
 // Operador ternário ? :
 const userPoints = 999;
 
-if (userPoints >= 1000) {
-    console.log(`Usuário VIP`);
-} else {
-    console.log(`Usuário comum`);
-}
+const userNivel = userPoints >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+console.log((userNivel));
+
+//if (userPoints >= 1000) {
+//    console.log(`Usuário VIP`);
+//} else {
+//    console.log(`Usuário comum`);
+//}
