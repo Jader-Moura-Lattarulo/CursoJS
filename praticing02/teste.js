91cb220f0fe0c2b6d6d1b56e78df3b2de77c952a
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-27 14:56:55 -0300

    Update teste.js

--- praticing02/teste.js
+++ praticing02/teste.js
@@ -5,10 +5,10 @@ console.log(arrayLetters);
 
 arrayLetters = arrayLetters.filter(letter => letter !== " ");
 
-function countFrequency(arrayLetters){
+function countFrequency(arrayLetters) {
     let frequency = {};
 
-    for (let i = 0; i < arrayLetters.length; i++){
+    for (let i = 0; i < arrayLetters.length; i++) {
         let letter = arrayLetters[i];
 
         if (frequency[letter]) {
@@ -18,9 +18,15 @@ function countFrequency(arrayLetters){
         }
     }
     return frequency;
-
 }
 
 let result = countFrequency(arrayLetters);
 
-console.log(result);
+let sortedResult = Object.entries(result).sort((a, b) => b[1] - a[1]);
+
+let sortedFrequency = {};
+sortedResult.forEach(([key, value]) => {
+    sortedFrequency[key] = value;
+});
+
+console.log(sortedFrequency);
