dc1fc8a33606526a1b6fe09e868cc4ffcb24ded5
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-27 11:58:02 -0300

    Update app03.js

--- praticing02/app03.js
+++ praticing02/app03.js
@@ -1,12 +1,31 @@
 function program(){
 
-    let arrayOfWords = ['Jader Moura Lattarulo', 'Gabriela Floreano Centenaro'];
-
+    let arrayOfWords = ['Jader Moura Lattarulo Gabriela Floreano Centenaro'];
+    let arrayLetters;
+    
     function turnToLetters(arrayOfWords){
-        let arrayLetters = arrayOfWords.split("");
-        return arrayLetters;  
+        arrayOfLetters = str.split('').map(char => `${char}`)       
     }
-    console.log(arrayLetters);
+
+    function countFrequency(arrayOfWords){
+        let frequency = {};
+
+        for (let i = 0; i < arrayOfWords.length; i++){
+            let letter = arrayOfWords[i];
+
+            if (frequency[letter]) {
+                frequency[letter]++;
+            } else {
+                frequency[letter] = 1;
+            }
+        }
+        return frequency;
+    }
+
+    arrayLetters = turnToLetters(arrayOfWords);
+    let result = countFrequency(arrayLetters);
+
+    console.log(result);
 
 }
 
