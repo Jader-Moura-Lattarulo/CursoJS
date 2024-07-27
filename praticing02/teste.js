ba83c6fbe5a259b3104f11c70713271b0943a967
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-27 15:34:03 -0300

    Update teste.js

--- praticing02/teste.js
+++ praticing02/teste.js
@@ -1,32 +1,38 @@
-let frase = ['Jader Moura Lattarulo', 'Gabriela Floreano Centenaro'];
-let combinedFrase = frase.join("");
-let arrayLetters = combinedFrase.split("");
-console.log(arrayLetters);
+function program(){
+    
+    let frase = ['Jader Moura Lattarulo', 'Gabriela Floreano Centenaro'];
+    let combinedFrase = frase.join("");
+    let arrayLetters = combinedFrase.split("");
+    console.log(arrayLetters);
 
-arrayLetters = arrayLetters.filter(letter => letter !== " ");
+    arrayLetters = arrayLetters.filter(letter => letter !== " ");
 
-function countFrequency(arrayLetters) {
-    let frequency = {};
+    function countFrequency(arrayLetters) {
+        let frequency = {};
 
-    for (let i = 0; i < arrayLetters.length; i++) {
-        let letter = arrayLetters[i];
+        for (let i = 0; i < arrayLetters.length; i++) {
+            let letter = arrayLetters[i];
 
-        if (frequency[letter]) {
-            frequency[letter]++;
-        } else {
-            frequency[letter] = 1;
+            if (frequency[letter]) {
+                frequency[letter]++;
+            } else {
+                frequency[letter] = 1;
+            }
         }
+        return frequency;
     }
-    return frequency;
+
+    let result = countFrequency(arrayLetters);
+
+    let sortedResult = Object.entries(result).sort((a, b) => b[1] - a[1]);
+
+    let sortedFrequency = {};
+    sortedResult.forEach(([key, value]) => {
+        sortedFrequency[key] = value;
+    });
+
+    console.log(sortedFrequency);
+
 }
 
-let result = countFrequency(arrayLetters);
-
-let sortedResult = Object.entries(result).sort((a, b) => b[1] - a[1]);
-
-let sortedFrequency = {};
-sortedResult.forEach(([key, value]) => {
-    sortedFrequency[key] = value;
-});
-
-console.log(sortedFrequency);
+program();
