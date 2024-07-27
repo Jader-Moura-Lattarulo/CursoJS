1d23ec8574cc1987764a1a063a318190c3e863de
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-27 14:50:30 -0300

    Update teste.js

--- praticing02/teste.js
+++ praticing02/teste.js
@@ -1,3 +1,26 @@
-let frase = "Jader Moura Lattarulo Gabriela Floreano Centenaro";
-let arrayLetras = frase.split("");
-console.log(arrayLetras);
+let frase = ['Jader Moura Lattarulo', 'Gabriela Floreano Centenaro'];
+let combinedFrase = frase.join("");
+let arrayLetters = combinedFrase.split("");
+console.log(arrayLetters);
+
+arrayLetters = arrayLetters.filter(letter => letter !== " ");
+
+function countFrequency(arrayLetters){
+    let frequency = {};
+
+    for (let i = 0; i < arrayLetters.length; i++){
+        let letter = arrayLetters[i];
+
+        if (frequency[letter]) {
+            frequency[letter]++;
+        } else {
+            frequency[letter] = 1;
+        }
+    }
+    return frequency;
+
+}
+
+let result = countFrequency(arrayLetters);
+
+console.log(result);
