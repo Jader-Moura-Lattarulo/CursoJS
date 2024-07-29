14a8e3a2b0df2025a436c6814b8dd0b06598fe2f
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-29 13:44:13 -0300

    Update js.js

--- exercicio-switch-date/assets/js/js.js
+++ exercicio-switch-date/assets/js/js.js
@@ -2,7 +2,12 @@ function app(){
     
     const todayIs = new Date();
     const dayOfWeek = todayIs.getDay();
+    const monthOfYear = todayIs.getMonth();
 
+
+    function zeroAEsquerda(num) {
+        return num >= 10 ? num : `0${num}`;
+    }
 
     function getDayOfWeek(dayOfWeek) {
         let dayOfWeekTxt;
@@ -35,13 +40,56 @@ function app(){
         }
     }
 
-    function setCompleteDate(dayOfWeekTxt, todayIs) {
-        
+    function getMonthOfYear(monthOfYear) {
+        let monthOfYearTxt;
+
+        switch(monthOfYear){
+            case 0:
+                monthOfYearTxt = 'Janeiro';
+                return monthOfYearTxt;
+            case 1:
+                monthOfYearTxt = 'Fevereiro';
+                return monthOfYearTxt;
+            case 2:
+                monthOfYearTxt = 'Março';
+                return monthOfYearTxt;
+            case 3:
+                monthOfYearTxt = 'Abril';
+                return monthOfYearTxt;
+            case 4:
+                monthOfYearTxt = 'Maio';
+                return monthOfYearTxt;
+            case 5:
+                monthOfYearTxt = 'Junho';
+                return monthOfYearTxt;
+            case 6:
+                monthOfYearTxt = 'Julho';
+                return monthOfYearTxt;
+            case 7:
+                monthOfYearTxt = 'Agosto';
+                return monthOfYearTxt;
+            case 8:
+                monthOfYearTxt = 'Setembro';
+                return monthOfYearTxt;
+            case 9:
+                monthOfYearTxt = 'Outubro';
+                return monthOfYearTxt;
+            case 10:
+                monthOfYearTxt = 'Novembro';
+                return monthOfYearTxt;
+            case 11:
+                monthOfYearTxt = 'Dezembro';
+                return monthOfYearTxt;
+            default:
+                monthOfYearTxt = '';
+                return monthOfYearTxt;
+        }
     }
 
     let dayOfWeekTxt = getDayOfWeek(dayOfWeek);
+    let monthOfYearTxt = getMonthOfYear(monthOfYear);
 
-    console.log(todayIs, dayOfWeekTxt);
+    console.log(dayOfWeekTxt, monthOfYearTxt);
 }
 
 app();
