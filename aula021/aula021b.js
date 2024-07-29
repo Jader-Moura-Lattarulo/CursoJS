3705e89783bfe99bd4d91170b5bea3c5f6e393b0
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-29 12:46:23 -0300

    update

---
+++ aula021/aula021b.js
@@ -0,0 +1,37 @@
+function getDayWeekTxt(diaSemana) {
+    let diaSemanaTexto;
+
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sábado';
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = '';
+            return diaSemanaTexto;
+    }   
+}
+
+
+const data = new Date('1989-02-22 00:00:01');
+const diaSemana = data.getDay();
+const diaSemanaTexto = getDayWeekTxt(diaSemana);
+
+console.log(diaSemana, diaSemanaTexto);
