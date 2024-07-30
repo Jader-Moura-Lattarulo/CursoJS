2a5405ca245bf6f7e1c62a125df468e95b1dd143
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-30 16:42:38 -0300

    Update triguinho.js

--- praticing04/triguinho.js
+++ praticing04/triguinho.js
@@ -1,14 +1,22 @@
 function app(){
     let max = 4;
     const min = 1;
-    const colors = [color01, color02, color03];
+
+    const colors = [];
 
     function colorMixer(){
-        let randomNumber = Math.random() * (max - min) + min;
-        return Math.floor(randomNumber)
+        let randomColor = Math.random() * (max - min) + min;
+        return Math.floor(randomColor)
     }
     
-    
+    function giveColor(){
+        for (let i = 0; i < 3; i++){
+            colors.push({ball: i+1, color:colorMixer()});
+        }
+    }
+
+    giveColor();
+    console.log(colors);
 }
 
 app();
