75106b742e11ad152d8b73d75693c60859565ade
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-30 13:09:23 -0300

    update

---
+++ exercicio-for/assets/js/js.js
@@ -0,0 +1,19 @@
+const elementos = [
+    {tag: 'p', texto: 'Frase 01'}, //0
+    {tag: 'div', texto: 'Frase 02'}, //1
+    {tag: 'section', texto: 'Frase 03'}, //2
+    {tag: 'footer', texto: 'Frase 04'}, //3
+];
+
+const container = document.querySelector('.container');
+const divCriada = document.createElement('div');
+
+for (let i = 0; i < elementos.length; i++) {
+    //console.log(elementos[i].tag);
+    let {tag, texto} = elementos[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerText = texto;
+    divCriada.appendChild(tagCriada);
+}
+
+container.appendChild(divCriada);
