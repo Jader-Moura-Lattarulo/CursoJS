a0fed22b1bb974c267189e064474fb5d2e940ecd
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-30 16:54:15 -0300

    Update triguinho.js

--- praticing04/triguinho.js
+++ praticing04/triguinho.js
@@ -1,16 +1,39 @@
 function app(){
-    let max = 4;
+    let numberOfBalls = 3;
+    const max = numberOfBalls + 1;
     const min = 1;
 
     const colors = [];
 
     function colorMixer(){
-        let randomColor = Math.random() * (max - min) + min;
+        let randomNumber = Math.random() * (max - min) + min;
+
+        switch (randomNumber) {
+            case 1:
+                randomColor = 'Vermelho';
+                return randomColor;
+            case 2:
+                randomColor = 'Azul';
+                return randomColor;
+            case 3:
+                randomColor = 'Amarelo';
+                return randomColor;
+            case 4:
+                randomColor = 'Verde';
+                return randomColor;
+            case 5:
+                randomColor = 'Branco';
+                return randomColor;
+            default:
+                randomColor = '';
+                return randomColor
+        }
+
         return Math.floor(randomColor)
     }
     
     function giveColor(){
-        for (let i = 0; i < 3; i++){
+        for (let i = 0; i < numberOfBalls; i++){
             colors.push({ball: i+1, color:colorMixer()});
         }
     }
