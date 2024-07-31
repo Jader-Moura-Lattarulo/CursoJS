ab1f7df8bcb96948d2374306f4e9a97c9dfaa8ae
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-31 12:48:15 -0300

    Update aula029b.js

--- aula029/aula029b.js
+++ aula029/aula029b.js
@@ -5,6 +5,7 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 for (let number of numbers){
     
     if (number === 5) {
+        console.log('5 encontrado, saindo...');
         break; // Breca a continuação do código
     }
     console.log(number);
