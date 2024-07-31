5881110a6d11fcc253bfe5a9290a3d9dd7a23dd2
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-31 12:38:51 -0300

    Create aula029.js

---
+++ aula029/aula029.js
@@ -0,0 +1,11 @@
+//Break e Continue - Funcionam em todos os laços
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let number of numbers){
+    
+    if (number === 5) {
+        continue; // pula para próxima ação do laço
+    }
+    console.log(number);
+}
