3aabad1ca014c4601b86d26b1f1991c1cdd4492c
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-31 12:46:06 -0300

    update

--- aula029/aula029b.js
+++ aula029/aula029b.js
@@ -4,13 +4,8 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 for (let number of numbers){
     
-    if (number === 2) {
-        console.log('Pulei o número 2');
-        continue; // pula para próxima ação do laço
-    }
-
     if (number === 5) {
-        continue; // pula para próxima ação do laço
+        break; // Breca a continuação do código
     }
     console.log(number);
 }
