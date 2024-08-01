ea0390b5c1373dc6c8abf491f5d6233bfb3a4176
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-01 11:27:33 -0300

    Update lista.js

--- exercicio-listadetarefas/assets/js/lista.js
+++ exercicio-listadetarefas/assets/js/lista.js
@@ -1,5 +1,23 @@
 function app(){
+    const userInput = document.querySelector('#todo-input');
+    const btnTodo = document.querySelector('.btn-todo');
+    const todoList = document.querySelector('.todo-list');
+
+    function createList() {
+        const li = document.createElement('li');
+        return li;
+    }
     
+    function createTodo(txtInput){
+        const li = createList();
+        li.innerHTML = txtInput;
+        todoList.appendChild(li);
+    }
+
+    btnTodo.addEventListener('click', function(){
+        if (!userInput.value) return; //if com uma linha só não preciso de chaves.
+        createTodo(userInput.value);
+    });
 
 }
 
