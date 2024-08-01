a1fa75d6c26fe4eab591b70faad1a16de209e608
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-01 13:34:30 -0300

    Update aula033.js

--- aula033/aula033.js
+++ aula033/aula033.js
@@ -26,3 +26,12 @@ const funcaoArrow = () => {
     console.log('Sou uma arrow function');
 }
 funcaoArrow();
+
+//Dentro de um objeto
+const obj = {
+    falar() {
+        console.log('Estou falando...');
+    }
+};
+
+obj.falar();
