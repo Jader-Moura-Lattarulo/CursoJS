45271c8bf1f5a75ae7dcf57abe7f4cd7dd0fce72
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-01 13:27:59 -0300

    Create aula033.js

---
+++ aula033/aula033.js
@@ -0,0 +1,27 @@
+//Várias maneiras de declarar funções
+
+//Function hoisting - A engine eleva as funções para o topo na hora da execução
+falaOi(); //Funciona por conta do hoisting
+function falaOi(){
+    console.log('Oi!');
+}
+
+//Funções são chamadas de First-class objects (Objetos de primeira classe)
+//Ou seja, podemos tratar funções como um dado.
+//Function expression
+const souUmDado = function(){
+    console.log('Sou um dado.');
+};
+souUmDado();
+
+function executaFunção(função) {
+    console.log('Vou executar sua função abaixo:');
+    função();
+}
+
+executaFunção(souUmDado);
+
+// Arrow function
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function');
+}
