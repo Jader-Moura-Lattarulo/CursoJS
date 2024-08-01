2d63a7e8ace49240daf3ca7835bdab5b574e0791
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-01 13:43:06 -0300

    Update aula034.js

--- aula034/aula034.js
+++ aula034/aula034.js
@@ -1,7 +1,13 @@
 //Parâmetros da função
+//Arguments sustenta todos os argumentos enviados para função - exclusivos para funções criadas com function
 
-function funcao(){
-    console.log('Alguma coisa');
+function funcao(a, b, c){
+    let total = 0;
+    for (let argumento of arguments) {
+        total += argumento;
+    }
+
+    console.log(total, a, b, c);
 }
 
-funcao('Valor');//enviando um argumento
+funcao(1, 2, 3, 4, 5, 6, 7, 8, 9);//enviando argumentos para funçao
