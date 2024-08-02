72548bbac4898d14eab67e7cbf1d90bc9ebc69b1
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-02 10:51:45 -0300

    update

--- aula040/aula040.js
+++ aula040/aula040.js
@@ -4,12 +4,27 @@ function criaPessoa(nome, sobrenome, altura, peso) {
     return {
         nome,
         sobrenome,
+        
+        //Getter
+        get nomeCompleto(){
+            return `${this.nome} ${this.sobrenome}`;
+        },
+
+        //Setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');
+            this.nome = valor.shift();
+            this.sobrenome = valor.join(' ');
+        },
+
         fala: function(assunto) {
             return `${this.nome} está ${assunto}.`; //this é sempre quem chama o método
         },
+        
         altura,
         peso,
-        imc(){
+        
+        get imc(){ //Getter transforma a função em um atributo da classe
             const indice = this.peso /(this.altura ** 2);
             return indice.toFixed(2);
         }
@@ -19,8 +34,12 @@ function criaPessoa(nome, sobrenome, altura, peso) {
 const p1 = criaPessoa('Jader', 'Moura', 1.70, 92);
 const p2 = criaPessoa('Gabi', 'Centenaro', 1.65, 75);
 
+p1.nomeCompleto = "Laura Lattarulo"
+
+console.log(p1.sobrenome);
+
 console.log(p1.fala('falando sobre JS')); //this se refere a p1
-console.log(p1.imc());
+console.log(p1.imc);
 
 console.log(p2.fala('falando sobre mestrado')); //this se refere a p2
-console.log(p2.imc());
+console.log(p2.imc);
