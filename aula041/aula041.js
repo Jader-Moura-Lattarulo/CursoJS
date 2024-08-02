118725d14ccbc352c816fdaedba4f743e07fab32
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-02 13:34:10 -0300

    Update aula041.js

--- aula041/aula041.js
+++ aula041/aula041.js
@@ -1,8 +1,27 @@
 //Funções construtoras -> objetos
 /**
  *  Iniciamos o nome da função com letra maiúscula
- *  Objeto(new)
+ * O corpo da função já é o objeto 
+ * Objeto(new)
+ * new -> cria um objeto vazio, faz o this apontar para esse objeto retornar direto para a variavel
  */
 //Funções fabrica -> objetos
 
-function Pessoa()
+function Pessoa(nome, sobrenome) {
+    const ID = 12345;
+    
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+
+    this.metodo = function(){
+        console.log(`${this.nome}`);
+        
+    };
+}
+
+const p1 = new Pessoa('Jader', 'Moura');
+const p2 = new Pessoa('Laura', 'Lattarulo');
+
+p1.metodo();
+
+console.log(p1.nome, p2.nome);
