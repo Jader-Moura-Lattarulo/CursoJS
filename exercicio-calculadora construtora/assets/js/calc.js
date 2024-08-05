dd800c12d5bf8827164239557efc0096706ca594
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-05 15:11:37 -0300

    Update calc.js

--- exercicio-calculadora construtora/assets/js/calc.js
+++ exercicio-calculadora construtora/assets/js/calc.js
@@ -1,25 +1,54 @@
-function app(){
-
-    function Calculadora(){
+function app() {
+    function Calculadora() {
         this.display = document.querySelector('.display');
         
-        this.capturaCliques = () => {
-            document.addEventListener('click', event => {
-                const el = event.target;
-                if(el.classList.contains('btn-num')) this.addNumDisplay(el);
+        this.inicia = () => {
+            this.capturaCliques();
+            this.capturaEnter();
+        };
+
+        this.capturaEnter = () => {
+            document.addEventListener('keypress', e => {
+                if (e.key !== 'Enter') return;
+                this.realizaConta();
             });
         };
 
-        this.addNumDisplay = el => this.display.value += el.innerText;
-
-        this.inicia = () => {
-            this.capturaCliques();
+        this.capturaCliques = () => {
+            document.addEventListener('click', event => {
+                const el = event.target;
+                if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+                if (el.classList.contains('btn-clear')) this.clear();
+                if (el.classList.contains('btn-del')) this.del();
+                if (el.classList.contains('btn-eq')) this.realizaConta();
+            });
         };
+
+        this.addNumDisplay = el => {
+            this.display.value += el.innerText;
+            this.display.focus();
+        };
+
+        this.clear = () => this.display.value = '';
+        this.del = () => this.display.value = this.display.value.slice(0, -1);
+        
+        this.realizaConta = () => {
+            try {
+                const conta = eval(this.display.value);
+
+                if (!conta && conta !== 0) { 
+                    alert('Conta inválida');
+                    return;
+                }
+                this.display.value = conta;
+            } catch(e) {
+                alert('Conta inválida');
+            }
+        }; 
     }
 
     const calculadora = new Calculadora();
     calculadora.inicia();
-
 }
 
 app();
