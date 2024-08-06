188ab0490a5dbbdf195c746672928cb29ad2ccce
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-06 10:14:03 -0300

    Update aula046.js

--- section-05/aula046/aula046.js
+++ section-05/aula046/aula046.js
@@ -1,7 +1,13 @@
 //Concatenando Arrays
+// ... rest -> captura o resto do array
+// ... spread -> espalha o array
+
 
 const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
-const array3 = array1.concat(array2);
+//const array3 = array1.concat(array2);
+const array3 = [...array1, 'Jader', ...array2, ...[7, 8, 9], ...'Jader'];
 
-console.log(array3);
+console.log(typeof(array1));
+
+console.log(array3, typeof(array3));
