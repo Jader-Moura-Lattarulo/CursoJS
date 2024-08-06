f8f174df3441531a79a0e9e02f65d9b0807033ec
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-06 08:16:38 -0300

    update

---
+++ aula043/aula043c.js
@@ -0,0 +1,22 @@
+//função geradora infinita
+
+function* geradora2() {
+    let i = 0;
+
+    while(true){
+        yield i;
+        i++
+    }
+}
+
+const g2 = geradora2();
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+console.log(g2.next().value);
+
+
