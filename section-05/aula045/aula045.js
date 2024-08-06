0c8aecd0a517bb68adb0cd4781c14ebcc3748ba3
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-06 09:54:02 -0300

    Create aula045.js

---
+++ section-05/aula045/aula045.js
@@ -0,0 +1,9 @@
+//Método Splice
+//               -5       -4       -3      -2      -1
+//                0        1        2       3       4
+const nomes = ['Jader', 'Marly', 'Laura', 'Gabi', 'Rita'];
+
+//nomes.splice(índice, quantos elementos eu quero deletar, elementos que quero add separados por virgula);
+const removidos = nomes.splice(0, 3);
+console.log(nomes, removidos);
+
