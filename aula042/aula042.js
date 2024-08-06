2d01a0927ca841005518be443e7555b732966d4a
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-06 08:01:02 -0300

    Update aula042.js

--- aula042/aula042.js
+++ aula042/aula042.js
@@ -1,5 +1,10 @@
 //Função recursivas - funções que se chamam de volta
 
 function recursiva(max) {
-    
+    console.log(max);
+    if (max >= 10) return; //encerra a função
+    max++; //encrementa
+    recursiva(max); //Se chama novamente, até passar da condição e vai embora.
 }
+
+recursiva(-10);
