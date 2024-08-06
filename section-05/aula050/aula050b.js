5b240c70f66657575737f42e74062e44702b57b9
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-06 14:27:50 -0300

    Update aula050b.js

--- section-05/aula050/aula050b.js
+++ section-05/aula050/aula050b.js
@@ -5,5 +5,9 @@
 //Reduzir
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 77, 11, 15, 22, 27];
 
-const tresEmUm = numeros.filter(valor => valor % 2 == 0).map(valor => valor * 2).reduce((acc, valor) => acc + valor, 0);
+const tresEmUm = numeros
+    .filter(valor => valor % 2 === 0)
+    .map(valor => valor * 2)
+    .reduce((acc, valor) => acc + valor, 0);
+
 console.log(tresEmUm);
