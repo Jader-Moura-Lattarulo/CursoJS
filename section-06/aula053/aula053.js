e6a53b0dfb202e61185ac61fa8c7b632ca364271
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-08 10:40:35 -0300

    Create aula053.js

---
+++ section-06/aula053/aula053.js
@@ -0,0 +1,16 @@
+//Object.defineProperty() e Object.defineProperties()
+
+function Produto(preco, estoque, nome) {
+    this.preco = preco;
+    this.estoque = estoque;
+    
+    Object.defineProperty(this, 'nome', {
+        enumerable: true, //mostra a chave
+        value: nome, //valor
+        writable: false, //permite ou não alterar o valor
+        configurable: false, //permite ou não reconfigurar a propriedade
+    });
+}
+
+const p1 = new Produto( 550, 5, "Camisa-SPFC");
+console.log(p1);
