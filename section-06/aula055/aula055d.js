6501a09c9813ba21395fdd2c953ae479751d4487
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-08 12:42:20 -0300

    Update aula055d.js

--- section-06/aula055/aula055d.js
+++ section-06/aula055/aula055d.js
@@ -1,3 +1,7 @@
 const produto = {nome: 'Produto', preco: 5.50, material: 'Plástico'};
 console.log(Object.values(produto));
 console.log(Object.entries(produto));
+
+for(let [chave, valor] of Object.entries(produto)) {
+    console.log(chave, valor);    
+}
