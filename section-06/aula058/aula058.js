111925bdc0ea45cfa3aa52cec99ca47a3880040e
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-12 12:42:30 -0300

    Create aula058.js

---
+++ section-06/aula058/aula058.js
@@ -0,0 +1,31 @@
+//Herança
+//Camiseta, caneca, lápis -> Produto
+//Produto -> aumento, deconsto
+
+function Produto(nome, preco){
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.aumento = function(quantia){
+    this.preco += quantia;
+};
+
+Produto.prototype.desconto = function(quantia){
+    this.preco -= quantia;
+};
+
+function Camiseta(nome, preco, cor){
+    Produto.call(this, nome, preco);
+}
+
+Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
+
+Camiseta.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+};
+
+const camiseta = new Camiseta('Regata', 45, 'Preta');
+camiseta.aumento(50);
+console.log(camiseta);
