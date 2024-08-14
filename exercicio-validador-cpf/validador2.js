7c4c479029437384c1d8e8ba9622589faa6ed2db
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-14 10:03:07 -0300

    Update validador2.js

--- exercicio-validador-cpf/validador2.js
+++ exercicio-validador-cpf/validador2.js
@@ -27,8 +27,11 @@ ValidaCPF.prototype.valida = function(){
     if(this.cpfLimpo.length !== 11) return false;
 
     const cpfParcial = this.cpfLimpo.slice(0, -2);
-     const digito1 = this.criaDigito(cpfParcial);
-    return true;
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+    const novoCpf = cpfParcial + digito1 + digito2;
+    
+    return novoCpf === this.cpfLimpo;
 };
 
 ValidaCPF.prototype.criaDigito = function(cpfParcial){
@@ -41,8 +44,7 @@ ValidaCPF.prototype.criaDigito = function(cpfParcial){
     }, 0);
 
     const digito = 11 - (total % 11);
-    console.log(digito);
-    
+    return digito > 9 ? '0' : String(digito);
 };
 
 const cpf = new ValidaCPF('705.484.450-52');
