d91d0930e02285a71795f79f0866f90df530929a
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-14 11:55:36 -0300

    Update aula060.js

--- section-06/aula060/aula060.js
+++ section-06/aula060/aula060.js
@@ -1,20 +1,27 @@
 //Factory Functions + Prototypes
 
+const falar ={
+    falar() {
+        console.log(`${this.nome} está falando`);
+    },
+};
+
+const comer ={
+    comer() {
+        console.log(`${this.nome} está comendo`);
+    },
+};
+
+const beber ={
+    beber() {
+        console.log(`${this.nome} está bebendo`);
+    },
+};
+
+//const pessoaPrototype = Object.assign({}, falar, comer, beber);
+const pessoaPrototype = { ...falar, ...comer, ...beber };
+
 function criaPessoa(nome, sobrenome){
-    const pessoaPrototype = {
-        falar() {
-            console.log(`${this.nome} está falando`);
-        },
-
-        comer() {
-            console.log(`${this.nome} está comendo`);
-        },
-
-        beber() {
-            console.log(`${this.nome} está bebendo`);
-        },
-    };
-
     return Object.create(pessoaPrototype, {
         nome: {value: nome},
         sobrenome: {value: sobrenome}
