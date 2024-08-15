7d34503ee636add1aa4b4699502bdac24b0212e9
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-15 09:37:14 -0300

    Create aula065.js

---
+++ section-07/aula065/aula065.js
@@ -0,0 +1,23 @@
+//Métodos de instância e estáticos
+
+class ControleRemoto {
+    constructor(tv){
+        this.tv = tv;
+        this.volume = 0;
+    }
+
+    aumentarVolume(){
+        this.volume += 2;
+    }
+
+    diminuirVolume(){
+        this.volume -=2;
+    }
+}
+
+const controle = new ControleRemoto('LG');
+controle.aumentarVolume();
+controle.aumentarVolume();
+controle.aumentarVolume();
+
+console.log(controle);
