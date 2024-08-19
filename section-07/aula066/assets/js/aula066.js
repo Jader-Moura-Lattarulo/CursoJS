8351a8a8972aee2cf894541c19fb634965c2655c
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-19 12:36:05 -0300

    update

--- section-07/aula066/assets/js/aula066.js
+++ section-07/aula066/assets/js/aula066.js
@@ -1,1 +1,29 @@
 //Validando um formulário (usando classe)
+class ValidaFormulario {
+    constructor(){
+        this.formulario = document.querySelector('.formulario')
+        this.eventos();
+    }
+
+    eventos(){
+        this.formulario.addEventListener('submit', e => {
+            this.handleSubmit(e);
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const validFields = this.isValid();        
+    }
+
+    isValid(){
+        let valid = true;
+
+        for(let field of this.formulario.querySelectorAll('.validar')){
+            console.log(field);
+            
+        }
+    }
+}
+
+const valida = new ValidaFormulario();
