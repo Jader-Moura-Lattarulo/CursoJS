8adc78d87a6a4452786887c869524f5cf5f57020
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-08-22 11:26:36 -0300

    Create aula067e.js

---
+++ section-08/aula067e.js
@@ -0,0 +1,39 @@
+//Resolve
+//Race
+//Métodos úteis para Promises
+
+function rand(min, max) {
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        if(typeof msg !== 'string'){
+            reject(false);
+            return;
+            }
+
+        setTimeout(() => {
+            resolve(msg.toUpperCase());
+            return;
+        }, tempo);
+    });
+}
+
+function baixaPagina(){
+    const emCache = true;
+
+    if(emCache){
+        return Promise.resolve('Página em cache');
+    } else {
+        return esperaAi('Baixei a página', rand(1, 3));
+    }
+}
+
+baixaPagina()
+    .then(dadosPagina => {
+        console.log(dadosPagina);
+    })
+    .catch(e => console.log(e));
