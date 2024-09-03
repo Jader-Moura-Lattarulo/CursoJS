f06b64dc02c85fbc0d8754e964006dedfd383ea4
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-09-03 13:55:26 -0300

    update

---
+++ node/aula005/app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const caminhoArquivo = path.resolve(__dirname, 'teste.json');
+const escreve = require('./modules/escrever');
+const ler = require('./modules/ler');
+
+//const pessoas = [
+//    {nome: 'Jader'},
+//    {nome: 'Gabi'},
+//    {nome: 'Laura'},
+//    {nome: 'Marly'},
+//    {nome: 'Dalva'}
+//];
+//
+//const json = JSON.stringify(pessoas, '', 2);
+//escreve(caminhoArquivo, json);
+
+async function leArquivo(caminho) {
+    const dados = await ler(caminho);
+    return dados;
+}
+
+const dadosArquivo = leArquivo(caminhoArquivo)
+    .then(dados => console.log(dados)
+);
