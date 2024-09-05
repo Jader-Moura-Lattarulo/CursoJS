47a0bdaff9c8f14b05a979eaaeadc0bbabcb706b
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-09-05 13:22:46 -0300

    update

---
+++ node/aula006/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+//https://meusite.com/ <- GET -> Entregue a página
+//https://meusite.com/sobre <- GET -> Entregue a página /sobre
+
+//CRUD -> Create, Read, Update, Delete
+//        Post    Get   Put     Delete
+
+app.get('/', (request, response) => {
+    response.send(`
+    <form action = "/" method = "POST">
+        Nome: <input type="text" name="nome"></input>
+        <button>Enviar</button>
+    </form>
+    `);
+});
+
+app.post('/', (request, response) => {
+    response.send('Recebi o formulário');
+});
+
+app.get('/contato', (request, response) => {
+    response.send('Obrigado por entrar em contato com a gente.');
+});
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    
+    console.log('Servidor executando na porta 3000');
+});
