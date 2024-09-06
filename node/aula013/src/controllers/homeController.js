0479cd054a9492edf989cb5eed2dd7320975e440
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-09-05 21:12:00 -0300

    update

--- node/aula013/src/controllers/homeController.js
+++ node/aula013/src/controllers/homeController.js
@@ -1,3 +1,12 @@
+const HomeModel = require('../models/HomeModel');
+
+HomeModel.create({
+    titulo: 'Um título de testes.',
+    descricao: 'Uma descrição de testes.'
+})
+    .then(dados => console.log(dados))
+    .catch(e => console.log(e));
+
 exports.paginaInicial = (req, res) => {
     console.log('Respondendo ao cliente');    
     res.render('index');
